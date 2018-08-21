8bdb0ed3756d3f839677121b5c9b8020e3301f32
liugogal <259394345@qq.com>
2018-08-21 23:47:35 +0800

    add: 新增录制接口

--- src/app.service.ts
+++ src/app.service.ts
@@ -40,7 +40,7 @@ export class AppService {
         this.stopRecord(channelId);
 
 
-        let recordCommandStr = `/home/code/Agora_Recording/samples/cpp/recorder_local --appId ${appId} --uid 0 --channel ${channelId} --appliteDir ${appliteDir} --isMixingEnable 1 --mixedVideoAudio 1`;
+        let recordCommandStr = `/home/code/Agora_Recording/samples/cpp/recorder_local --appId ${appId} --uid 0 --channel ${channelId} --appliteDir ${appliteDir} --isMixingEnable 1 --mixedVideoAudio 1 &`;
 
         let process: ChildProcess = exec(recordCommandStr, (error: Error | null, stdout: string, stderr: string) => {
             console.log(`stdout:${stdout}`);
